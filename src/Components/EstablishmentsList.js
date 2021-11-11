6f10bb43e563448e4f9e7dd0ef17ca91c0b3d7fc
Victor <victorgvargas>
2021-11-11 16:41:00 -0300

    Added axios

---
+++ src/Components/EstablishmentsList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListSubheader from "@mui/material/ListSubheader";
+import Avatar from "@mui/material/Avatar";
+
+export default function EstablishmentsList() {
+  return (
+    <List>
+      <ListSubheader>Estabelecimentos</ListSubheader>
+    </List>
+  );
+}
