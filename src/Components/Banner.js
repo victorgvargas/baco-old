d569f257963680f30eb155898235491eb7ef942c
Victor Vargas <vvargas2013@gmail.com>
2021-11-12 18:37:31 -0300

    Adjusted carousel indicators

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -10,12 +10,39 @@ const Img = styled.img`
 
 const Box = styled.div`
   padding: 7px;
+  text-align: center;
 `;
 
 export default function Banner({ imgUrls }) {
   return (
     <Box>
-      <Carousel showThumbs={false} showArrows={false}>
+      <Carousel 
+        showThumbs={false} 
+        showArrows={false} 
+        infiniteLoop 
+        renderIndicator={(onClickHandler, isSelected, index, label) => {
+          const style = {
+            margin: '0 1px',
+            backgroundColor: '#000000', 
+            borderRadius: '50%',
+            cursor: 'pointer' 
+          };
+          const activeStyle = isSelected ? {...style, border: '1px solid #e65100', boxShadow: '0 2px 2px #666'} : {...style};
+
+          return (
+            <span
+              style={activeStyle}
+              onClick={onClickHandler}
+              onKeyDown={onClickHandler}
+              value={index}
+              key={index}
+              role="button"
+              tabIndex={0}
+              aria-label={`${label} ${index + 1}`}
+            >&emsp;
+            </span>
+          );
+        }}>
         {imgUrls.map((url, index) => (
           <div key={index}>
             <Img src={url} alt="Banner"></Img>
